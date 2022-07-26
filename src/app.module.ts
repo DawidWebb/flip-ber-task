dc8985f5a124127a5c619a196afb08b0ef92d420
DawidWebb <tomasz.matras@developerweb.pl>
2022-07-26 13:38:35 +0200

    connect app with DB, add mongosse dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Top10profProdModule } from './top10prof-prod/top10prof-prod.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_DB_CONNECT), Top10profProdModule],
   controllers: [AppController],
   providers: [AppService],
 })
